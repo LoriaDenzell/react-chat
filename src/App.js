81ee42c68057c28a4682c4d7e1eea365b0d6d3a3
LoriaDenzell <loriadenzell@gmail.com>
2021-03-04 19:17:46 -0800

    First commit for this live chat react app.

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '649df6d9-ebf2-4bb6-99c5-db3640260ac8';
+
+const App = () => {
+	if (!localStorage.getItem('username')) return <LoginForm />;
+
+    return(
+        <ChatEngine
+			height="100vh"
+			projectID={projectID}
+			userName={localStorage.getItem('username')}
+			userSecret={localStorage.getItem('password')}
+			renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+			onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+		/>
+    ); 
+};
+
+export default App;
